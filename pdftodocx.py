d9f48ef165b39f87e1dccb20d6e1634e37ad0f5f
Javascript Don <donlevans29@gmail.com>
2025-11-09 09:09:53 -0500

    commit core documents to start github repo

---
+++ pdftodocx.py
@@ -0,0 +1,9 @@
+from pdf2docx import Converter
+
+pdf_file = 'resume.pdf'
+docx_file = 'output/resume.docx'
+
+# convert pdf to docx
+cv = Converter(pdf_file)
+cv.convert(docx_file)      # all pages by default
+cv.close()
